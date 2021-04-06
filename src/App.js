4ddfcfcb9dcb796ce10d95af5f3d55e6abab3b95
David Worms <david@adaltas.com>
2021-04-06 14:49:29 +0200

    feat: rename query to dbnomics

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import "./App.css";
 
 const DEFAULT_QUERY = `# shift-option/alt-click on a query below to jump to it in the explorer
 # option/alt-click on a field in the explorer to select all subfields
-query adaltas {
+query dbnomics {
   # Retun the list of providers present inside DBnomics
   get_providers {
     nb_datasets
